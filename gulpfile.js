ec452c33ad64297a3d6a3261f3602bcede1104be
Daniil <example@gmail.com>
2022-11-18 23:09:20 +0300

    second part

--- gulpfile.js
+++ gulpfile.js
@@ -17,12 +17,12 @@ gulp.task('server', function() {
 });
 
 gulp.task('styles', function() {
-    return gulp.src("src/sass/**/*.+(scss|sass)")
+    return gulp.src('src/sass/**/*.+(scss|sass)')
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename({suffix: '.min', prefix: ''}))
         .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("src/css"))
+        .pipe(gulp.dest('src/css'))
         .pipe(browserSync.stream());
 });
 
